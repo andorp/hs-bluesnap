68aedd4436ed99ca79babeabf63793c65cc1fd53
Andor Penzes <andor.penzes@gmail.com>
2015-05-06 19:53:43 +0200

    Test order XMLs.

--- test/test-suite.hs
+++ test/test-suite.hs
@@ -1,11 +1,33 @@
 module Main where
 
+import Control.Applicative ((<$>))
+import Data.List (isSuffixOf)
+import System.Directory
+import System.FilePath.Posix ((</>))
+
 import Test.Tasty
 import Test.Tasty.HUnit
 import Bluesnap.Test.Parser
 
-main = defaultMain tests
+main = do
+  responseOrderTests <- readTestData "Order Responses" "test/data/response/order/" testOrderResponseParser
+  requestOrderTests  <- readTestData "Order Request"   "test/data/request/order/"  testOrderRequestParser
+  defaultMain $ testGroup "Tests" [
+      responseOrderTests
+    , requestOrderTests
+    ]
 
+readTestData name dir parserTest = do
+  xmls <- filter (isSuffixOf ".xml") . filter (not . flip elem [".", ".."]) <$> getDirectoryContents dir
+  print xmls
+  return
+    $ testGroup name
+    $ map (\name -> let name' = dir </> name
+                    in testCase name' $ parserTest name')
+      xmls
+
+
+{-}
 tests :: TestTree
 tests = testGroup "Tests" [responseParseTests, requestParseTests]
 
@@ -16,3 +38,4 @@ responseParseTests = testGroup "Response parse tests"
 requestParseTests = testGroup "Request parse tests"
   [ testCase "order.xml" $ testOrderRequestParser "test/data/request/order.xml"
   ]
+-}
