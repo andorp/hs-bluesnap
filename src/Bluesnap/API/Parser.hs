98ca07f87720fd35ad0bb44f1c3c703730b657a7
Andor Penzes <andor.penzes@gmail.com>
2015-05-25 00:52:13 +0200

    Save functionality for Client API

--- src/Bluesnap/API/Parser.hs
+++ src/Bluesnap/API/Parser.hs
@@ -1,17 +1,23 @@
 module Bluesnap.API.Parser (
     parse
+  , render
   , SchemaType(..)
   , XMLParser
+  , Content(..)
   ) where
 
-import Text.XML.HaXml.XmlContent
-import Text.XML.HaXml.Lex
-import Text.XML.HaXml.Parse
-import Text.XML.HaXml.Posn
-import Text.XML.HaXml.Schema.Schema (SchemaType(..))
-import Text.XML.HaXml.XmlContent.Parser (XMLParser)
+import qualified Text.PrettyPrint as Pretty
+import           Text.XML.HaXml.XmlContent
+import           Text.XML.HaXml.Lex
+import           Text.XML.HaXml.Parse
+import           Text.XML.HaXml.Posn
+import           Text.XML.HaXml.Pretty as XMLPretty
+import           Text.XML.HaXml.Schema.Schema (SchemaType(..))
+import           Text.XML.HaXml.Types
+import           Text.XML.HaXml.XmlContent.Parser (XMLParser)
 
 parse p = fst . runParser p . getXmlContents . xmlParse "req-rsp"
   where
     getXmlContents (Document _ _ e _) = [CElem e noPos]
 
+render = Pretty.render . XMLPretty.content
--- src/Bluesnap/Client/API.hs
+++ src/Bluesnap/Client/API.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
-module Bluesnap.Client.API where {- (
-    get
-  , save
-  , update
-  ) where -}
+module Bluesnap.Client.API where
+
+import           Data.Char (toLower)
+import           Data.List (find)
 
 import           Bluesnap.API.Parser
 import qualified Bluesnap.API.Request  as Request
@@ -14,21 +13,24 @@ import qualified Bluesnap.Client as Client (get, post, put)
 
 class Object o where
   data ObjectID o
-  type ObjectRequest o
-  type ObjectResponse o
+  data ObjectRequest o
+  data ObjectResponse o
   servicePath  :: o -> Path
   idToPath :: ObjectID o -> Path
   responseParser :: o -> XMLParser (ObjectResponse o)
-  requestParser :: o -> XMLParser (ObjectRequest o)
   fromResponse :: ObjectResponse o -> Either String o
   toRequest :: o -> ObjectRequest o
+  requestToContent :: ObjectRequest o -> [Content ()]
+  oidFromLocation :: String -> ObjectID o
 
+-- | Type inference trick to get out the object type from a given object id type
 fakeObj :: (Object o) => ObjectID o -> o
 fakeObj = error "fakeObj :: (Object o) => ObjectID o -> o"
 
 fullPath :: (Object o) => ObjectID o -> Path
 fullPath oid = concat [servicePath (fakeObj oid), idToPath oid]
 
+-- | Gets the object from Bluesnap based on the given id.
 get :: Object o => (ObjectID o) -> Bluesnap o
 get oid = do
   let parser = responseParser (fakeObj oid)
@@ -36,22 +38,40 @@ get oid = do
   responseXML <- checkParse $ parse parser payload
   checkParse $ fromResponse responseXML
 
+-- | Saves the object in Bluesnap and returns its id based on
+-- the location header.
 save :: Object o => o -> Bluesnap (ObjectID o)
-save obj = error "save :: Object o => o -> Bluesnap (ObjectID o)"
+save obj = do
+  payload <- fmap render . safeHead . requestToContent $ toRequest obj
+  (headers, payload) <- Client.put (servicePath obj) payload
+  location <- getLocationValue headers
+  return $ oidFromLocation location
+  where
+    safeHead :: [a] -> Bluesnap a
+    safeHead []    = throwError $ BluesnapError "safeHead failed"
+    safeHead (a:_) = return a
 
 update :: Object o => (ObjectID o) -> o -> Bluesnap ()
 update oid obj = error "update :: Object o => (ObjectID o) -> o -> Bluesnap ()"
+
+-- | Get the value of location header or throws a Bluesnap error
+getLocationValue :: [Header] -> Bluesnap String
+getLocationValue hs = maybe
+  (throwError $ BluesnapError "Location header is not found!")
+  (return . snd)
+  (find (("location"==) . map toLower . fst) hs)
 
 data Subscription = Subscription String
   deriving (Eq, Show)
 
 instance Object Subscription where
   data ObjectID Subscription = SubscriptionID String
-  type ObjectRequest Subscription = Request.Subscription
-  type ObjectResponse Subscription = Response.Subscription
+  data ObjectRequest Subscription = SubsReq { unSubsReq :: Request.Subscription }
+  data ObjectResponse Subscription = SubsResp { unSubsResp :: Response.Subscription }
   servicePath _ = "/services/2/subscriptions/"
   idToPath (SubscriptionID value) = value 
-  responseParser _ = Response.elementSubscription
-  requestParser _ = Request.elementSubscription
-  fromResponse r = Right $ Subscription (show r)
+  responseParser _ = fmap SubsResp $ Response.elementSubscription
+  fromResponse (SubsResp r) = Right $ Subscription (show r)
   toRequest s = error "toRequest s"
+  requestToContent = Request.elementToXMLSubscription . unSubsReq
+  oidFromLocation = SubscriptionID -- TODO: Last part of the path
