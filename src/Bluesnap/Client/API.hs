29a68d411cf0b0dae801032cc37849631a297372
Andor Penzes <andor.penzes@gmail.com>
2015-05-26 00:07:54 +0200

    Renaming get, save to retrieve, create and first version of update.

--- src/Bluesnap/Client/API.hs
+++ src/Bluesnap/Client/API.hs
@@ -1,15 +1,25 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
-module Bluesnap.Client.API where
+module Bluesnap.Client.API (
+    Object(..)
+  , retrieve
+  , create
+  , update
+  ) where
+
+import           Prelude hiding (last)
 
 import           Data.Char (toLower)
 import           Data.List (find)
+import           Data.List.Split (splitOn)
 
 import qualified Bluesnap.API.Request  as Request
 import qualified Bluesnap.API.Response as Response
 import           Bluesnap.API.XML
 import           Bluesnap.Client hiding (get, post, put)
 import qualified Bluesnap.Client as Client (get, post, put)
+
+import           Network.URI
 
 class Object o where
   data ObjectID o
@@ -31,8 +41,8 @@ fullPath :: (Object o) => ObjectID o -> Path
 fullPath oid = concat [servicePath (fakeObj oid), idToPath oid]
 
 -- | Gets the object from Bluesnap based on the given id.
-get :: Object o => (ObjectID o) -> Bluesnap o
-get oid = do
+retrieve :: Object o => (ObjectID o) -> Bluesnap o
+retrieve oid = do
   let parser = responseParser (fakeObj oid)
   payload <- Client.get (fullPath oid)
   responseXML <- checkParse $ parse parser payload
@@ -40,26 +50,51 @@ get oid = do
 
 -- | Saves the object in Bluesnap and returns its id based on
 -- the location header.
-save :: Object o => o -> Bluesnap (ObjectID o)
-save obj = do
+create :: Object o => o -> Bluesnap (ObjectID o)
+create obj = do
   payload <- fmap render . safeHead . requestToContents $ toRequest obj
-  (headers, payload) <- Client.put (servicePath obj) payload
+  (headers, payload) <- Client.post (servicePath obj) payload
   location <- getLocationValue headers
   return $ oidFromLocation location
-  where
-    safeHead :: [a] -> Bluesnap a
-    safeHead []    = throwError $ BluesnapError "safeHead failed"
-    safeHead (a:_) = return a
 
+-- | Updates the object in Bluesnap for the given object id and
+-- returns unit if the update was successful otherwise throws a
+-- bluesnap error
 update :: Object o => (ObjectID o) -> o -> Bluesnap ()
-update oid obj = error "update :: Object o => (ObjectID o) -> o -> Bluesnap ()"
+update oid obj = do
+  payload <- fmap render . safeHead . requestToContents $ toRequest obj
+  _ <- Client.put (fullPath oid) payload
+  return ()
+
+-- * Helpers
+
+-- | Get the head of the list or throws a bluesnap error if the list is empty
+safeHead :: [a] -> Bluesnap a
+safeHead []    = throwError $ BluesnapError "safeHead failed"
+safeHead (a:_) = return a
 
 -- | Get the value of location header or throws a Bluesnap error
-getLocationValue :: [Header] -> Bluesnap String
-getLocationValue hs = maybe
+getIDFromLocation :: [Header] -> Bluesnap String
+getIDFromLocation headers = maybe
   (throwError $ BluesnapError "Location header is not found!")
-  (return . snd)
-  (find (("location"==) . map toLower . fst) hs)
+  return
+  (maybeIDFromLocation headers)
+
+-- EG: maybeIDFromLocation [("Location", "https://sandbox.bluesnap.com/services/2/shoppers/19575974")]
+-- Just "19575974"
+maybeIDFromLocation :: [Header] -> Maybe String
+maybeIDFromLocation headers =
+  ((find (("location"==) . map toLower . fst) headers) >>= (return . snd) >>= parseURI >>= getOID)
+  where
+    getOID :: URI -> Maybe String
+    getOID = last . splitOn "/" . uriPath
+
+last :: [a] -> Maybe a
+last []     = Nothing
+last [a]    = Just a
+last (_:as) = last as
+
+-- * Examples
 
 data Subscription = Subscription String
   deriving (Eq, Show)
